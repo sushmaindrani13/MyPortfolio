3e32061117f205ecde651c1ad9ebe4a7e5b1dd44
Sushma Indrani <sushma@Sushmas-MacBook-Pro.local>
2024-12-10 14:45:45 -0500

    minor edits

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { FaLinkedinIn } from 'react-icons/fa';
+import { HiMail } from 'react-icons/hi'; // Import mail icon
 
 const FooterBottom = () => {
-  // Replace 'your_linkedin_profile_url' with your actual LinkedIn profile URL
+  // Replace with your actual LinkedIn profile URL and email
   const linkedinUrl = 'https://www.linkedin.com/in/sushma-indrani/';
+  const email = 'sushmaindrani2425@gmail.com';
 
   return (
     <div className="w-full py-3 flex flex-col items-center justify-center">
       <div className="flex gap-4">
-        <a href={linkedinUrl} target="_blank" rel="noopener noreferrer" className="bannerIcon">
+        <a
+          href={linkedinUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bannerIcon"
+        >
           <FaLinkedinIn />
         </a>
+        <a
+          href={`mailto:${email}`}
+          className="bannerIcon"
+        >
+          <HiMail />
+        </a>
       </div>
-      <br></br>
+      <br />
       <p className="text-center text-purple-300 text-base">
         © 2024. All rights reserved by Dangeti Sushma Indrani
       </p>
     </div>
   );
-}
+};
 
 export default FooterBottom;
